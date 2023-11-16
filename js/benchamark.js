535c0871f26f33b45e20e97f38ba26d367b0197d
0znerol <lorenzo.pezzica98@gmail.com>
2023-11-16 16:18:03 +0100

    modified:   js/benchamark.js

--- js/benchamark.js
+++ js/benchamark.js
@@ -98,31 +98,46 @@ const questions = [
     incorrect_answers: ["Python", "C", "Jakarta"],
   },
 ];
+//funzione che parte al cariamento della pagina
 window.onload = function () {
   loadQ(questionCounter);
+
+  //click event listener per le risposte
   risposte.addEventListener("click", (event) => {
+    // \/ viene dichiarato qua perche dopo verra eliminato e reinserito, se fosse stato dichiarato
+    // \/ globalmente non sarebbe riuscito a selezionare gli svg generati dopo la prima domanda
     let timerContainer = document.querySelector("svg");
+    // \/ duplica, rimpiazza e ri-seleziona il timer al click
     let duplicate = timerContainer.cloneNode(true);
     timerContainer.parentNode.replaceChild(duplicate, timerContainer);
     timerElement = document.getElementById("timer");
+    //
+
     nextQuestion(event, questionCounter);
     correctACheck(event, questionCounter);
     updateCountdown(questionCounter);
+
+    // \/ viene dichiarato qua per la stessa ragione di timerContainer
     timerElement.textContent = 10;
   });
 };
 //controlla se una risposta è corretta
 function correctACheck(event, qc) {
+  //assegna il testo del elemento che è stato cliccato a isCorrect
   const isCorrect = event.target.innerHTML;
-  console.log(qc);
-  console.log(isCorrect);
+  // console.log(qc);
+  // console.log(isCorrect);
+  // se isCorrect è uguale alla proprieta correct_answer aumenta correctCounter di 1
+  console.log(correctCounter);
+
   if (isCorrect === questions[qc - 1].correct_answer) {
     correctCounter++;
     console.log(correctCounter);
   }
 }
 
-//inserice domanda per domanda all'interno del suo div ogni volta che viene chiamato
+//inserice domanda per domanda all'interno del suo div e chiama la
+//funzione per generare le risposte ogni volta che viene chiamato
 function loadQ(qc) {
   let singleQuestion = questions[qc].question;
   document.querySelector("#domanda").innerHTML = `<p>${singleQuestion}</p>`;
@@ -150,12 +165,13 @@ function loadQ(qc) {
 // }
 //funzione per passare alla domanda succesiva al click
 function nextQuestion(event, qc) {
+  // assegna il "tipo"(nome del nodo) dell'elemento cliccato
   const isButton = event.target.nodeName;
   console.log(isButton);
+  //controlla se viene effetivamente cliccato un label, dato che
+  //l'eventListener è su tutta la div
   if (isButton === "LABEL") {
     console.log(isButton);
-    //controlla se viene effetivamente cliccato un bottone, dato che
-    //l'eventListener è su tutta la div
     qc++;
     questionCounter++;
     if (qc === 10) {
@@ -177,27 +193,33 @@ function genButton(qc) {
     //ciclo per creare tanti bottoni quante risposte
     let inputCreate = document.createElement("input");
     let labelCreate = document.createElement("label");
-    inputCreate.id = `questionInput${i}`;
+    // inputCreate.id = `questionInput${i}`;
     risposte.appendChild(labelCreate);
     labelCreate.appendChild(inputCreate);
   }
+
   let input = document.querySelectorAll("input");
   let lable = document.querySelectorAll("label");
   for (i = 0; i < answersNum; i++) {
     input[i].type = "radio";
-    //crea un numero random che rapressenta il lable dove
-    //viene inserita una risposta
+    console.log(input[i]);
+    //crea un numero random tra 0 ed il numero di risposte,
+    //che rapressenta il lable dove viene inserita una risposta
     let rand = Math.floor(Math.random() * answersNum);
     //console.log(usedButton);
+    // se il numero random non è incluso nell'array useButton
     if (!usedButton.includes(rand)) {
       usedButton.push(rand);
-      lable[rand].innerText = questions[qc].incorrect_answers[i];
+      lable[rand].innerText += questions[qc].incorrect_answers[i]; //assegna le risposte sbagliate in un lable a caso
     } else {
+      //se il numero random generato è gia inserito nell'array ripeti il ciclo senza
+      //incrementare
       i--;
     }
   }
   for (i = 0; i < answersNum; i++) {
     // console.log(i);
+    //controlla i lable e assegna la risposta correta all'ultimo undefined rimasto
     if (lable[i].innerText === "undefined") {
       lable[i].innerText = questions[qc].correct_answer;
       break;
@@ -208,16 +230,22 @@ function genButton(qc) {
 //TIMER
 
 function updateCountdown(qc) {
-  let seconds = parseInt(timerElement.textContent);
+  let seconds = 10000000; //parseInt(timerElement.textContent);
+  // \/ salva correctCounter nello storage locale del browser
+  // \/ per accederci da un altro .js
   localStorage.setItem("result", correctCounter);
-  console.log(localStorage.getItem("result"));
-  console.log(seconds);
+
+  // console.log(localStorage.getItem("result"));
+  // console.log(seconds);
   if (seconds > 1) {
     seconds--;
     timerElement.textContent = seconds;
   } else if (qc === 9) {
+    // se il contatore di domande arriva al limite apri result
     window.location.href = "../html/results.html";
   } else {
+    //se secondi arriva al limite incrementa il contatore delle domande
+    //e resetta i vari elementi che vanno ricaricati
     risposte.innerHTML = "";
     timerElement.textContent = 10;
     qc++;
