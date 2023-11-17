8911f4493321d2213095764f7bc1e52eb3475f17
Celine Fatima Emanuele <celine.emanuele03@gmail.com>
2023-11-17 14:15:28 +0100

    nuovo

--- js/welcome.js
+++ js/welcome.js
@@ -1,15 +1,23 @@
-const checkbox = document.querySelector('input');
-const button = document.getElementById('bottone');
 
+// seleziona l'elemento input  della pagina  e lo assegna alla variabile checkbox.
+const checkbox = document.querySelector('input');
+// seleziona l'elemento con ID 'bottone' e lo assegna alla variabile button, che è un elemento pulsante 
+const button = document.getElementById('bottone');
+//questa riga inizialmente disabilita il pulsante al caricamento della pagina.
 button.disabled = true
+
+
 
  checkbox.addEventListener('change', function() {
 
-      if (checkbox.checked) {
+      if (checkbox.checked) { // controlla se la casella  è selezonata
 
-        button.disabled = false;
+        button.disabled = false;//se la casella è selezionata abilita il pulsante
       } else {
         
-        button.disabled = true;
+        button.disabled = true;// se non è selezionata disabilita il pulsante 
       }
     });
+
+    // la funzionalità è che quando la casella di controllo è selezionata, 
+    //il pulsante viene abilitato e quando è deselezionata, il pulsante diventa disabilitato.
