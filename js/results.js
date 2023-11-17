4324cb2dbe2b7672bd270fc31ca1a2c472ed440d
0znerol <lorenzo.pezzica98@gmail.com>
2023-11-17 09:27:26 +0100

    modified:   js/results.js

--- js/results.js
+++ js/results.js
@@ -1,134 +1,133 @@
 //Link pulsante Rate Us a recensioni
-
-let btn = document.querySelector('button');
- btn.addEventListener('click', feedbackPage);
+let btn = document.querySelector("button");
+btn.addEventListener("click", feedbackPage);
 
 function feedbackPage() {
-    window.location.href="../html/FeedbackPage.html";
-  };
-  
-  //CHART
+  window.location.href = "../html/FeedbackPage.html";
+}
 
-  let correct = localStorage.getItem("result");
-  let sbagliati = 10 - correct;
+//CHART
 
-  console.log(correct);
+let correct = localStorage.getItem("result");
+let sbagliati = 10 - correct;
 
-  var xValues = ["Wrong","Correct"];
-  var yValues = [sbagliati, correct];
-  var barColors = [ "#D20094","#00FFFF"];
-  
-  new Chart("myChart", {
-    type: "doughnut",
-    data: {
-      labels: xValues,
-      datasets: [
-        {
-          backgroundColor: barColors,
-          data: yValues,
-          borderColor: "none",
-          borderWidth: 0,
-        },
-      ],
-    },
-    options: {
-      // plugins: {
-      //   doughnutLabel:{
-      //     draw: function() {
-      //       Chart.types.Doughnut.prototype.draw.apply(this, arguments);
-    
-      //       var width = this.chart.width,
-      //           height = this.chart.height;
-    
-      //       var fontSize = (height / 114).toFixed(2);
-      //       this.chart.ctx.font = fontSize + "em Verdana";
-      //       this.chart.ctx.textBaseline = "middle";
-    
-      //       var text = function testoCOS(){
-      //         if(correct > sbagliati){
-      //           return ""
-      //         } else {
-  
-      //         }
-      //       },
-      //           textX = Math.round((width - this.chart.ctx.measureText(text).width) / 2),
-      //           textY = height / 2;
-    
-      //       this.chart.ctx.fillText(text, textX, textY);
-      //   }
-      //   }
-      // },
-      title: {
-        display: false,
-        text: "Donut Chart Results",
+console.log(correct);
+
+var xValues = ["Wrong", "Correct"];
+var yValues = [sbagliati, correct];
+var barColors = ["#D20094", "#00FFFF"];
+
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        backgroundColor: barColors,
+        data: yValues,
+        borderColor: "none",
+        borderWidth: 0,
       },
-      cutoutPercentage: 70,
-      legend: {
-        display: false,
-      },
+    ],
+  },
+  options: {
+    // plugins: {
+    //   doughnutLabel:{
+    //     draw: function() {
+    //       Chart.types.Doughnut.prototype.draw.apply(this, arguments);
+
+    //       var width = this.chart.width,
+    //           height = this.chart.height;
+
+    //       var fontSize = (height / 114).toFixed(2);
+    //       this.chart.ctx.font = fontSize + "em Verdana";
+    //       this.chart.ctx.textBaseline = "middle";
+
+    //       var text = function testoCOS(){
+    //         if(correct > sbagliati){
+    //           return ""
+    //         } else {
+
+    //         }
+    //       },
+    //           textX = Math.round((width - this.chart.ctx.measureText(text).width) / 2),
+    //           textY = height / 2;
+
+    //       this.chart.ctx.fillText(text, textX, textY);
+    //   }
+    //   }
+    // },
+    title: {
+      display: false,
+      text: "Donut Chart Results",
     },
-  });
-  
-  
-  let risultatoCorretto = document.querySelector('#Corretto p')
-   function testoCorretti() {
-    risultatoCorretto.innerText= correct;
-   };
-  
-   testoCorretti();
-  
-   let risultatoSbagliato = document.querySelector('#Sbagliato p')
-   function testoSbagliato() {
-    risultatoSbagliato.innerText= sbagliati;
-   };
-  
-   testoSbagliato();
-  
-   let fraseCorretto = document.querySelector('#Corretto .TestoPiccolo')
-  
-   function risultatoConSlashCorretto() {
-    fraseCorretto.innerText = correct + "/10 questions";
-   }
-  
-   risultatoConSlashCorretto();
-  
-   let fraseNonCorretto = document.querySelector('#Sbagliato .TestoPiccolo')
-  
-   function risultatoConSlashNonCorretto() {
-    fraseCorretto.innerText = sbagliati + "/10 questions";
-   }
-  
-  risultatoConSlashNonCorretto();
-  
-  let testoCentro = document.querySelector('#centerText')
-  
-  function testoAlCentro(){
-    if(correct > sbagliati){
-      let p1 = document.createElement('p');
-      let p2 = document.createElement('p');
-      let p3 = document.createElement('p');
-  
-      p1.innerText= "Congratulations!";
-      p2.innerText= "You passed the exam";
-      p3.innerText="We'll send you the certificate in a few minutes. Check your email (including promotions / spam folder)";
-  
-      testoCentro.appendChild(p1);
-      testoCentro.appendChild(p2);
-      testoCentro.appendChild(p3);
-    } else {
-      let p1 = document.createElement('p');
-      let p2 = document.createElement('p');
-      let p3 = document.createElement('p');
-  
-      p1.innerText= "Better luck next time!";
-      p2.innerText= "You failed the exam";
-      p3.innerText= "Check-in with your teaching assistant for more informations on how to proceed from here.";
-  
-      testoCentro.appendChild(p1);
-      testoCentro.appendChild(p2);
-      testoCentro.appendChild(p3);
-    }
+    cutoutPercentage: 70,
+    legend: {
+      display: false,
+    },
+  },
+});
+
+let risultatoCorretto = document.querySelector("#Corretto p");
+function testoCorretti() {
+  risultatoCorretto.innerText = `${(correct / 10) * 100}%`;
+}
+
+testoCorretti();
+
+let risultatoSbagliato = document.querySelector("#Sbagliato p");
+function testoSbagliato() {
+  risultatoSbagliato.innerText = `${(sbagliati / 10) * 100}%`;
+}
+
+testoSbagliato();
+
+let fraseCorretto = document.querySelector("#Corretto .TestoPiccolo");
+
+function risultatoConSlashCorretto() {
+  fraseCorretto.innerText = correct + "/10 questions";
+}
+
+risultatoConSlashCorretto();
+
+let fraseNonCorretto = document.querySelector("#Sbagliato .TestoPiccolo");
+
+function risultatoConSlashNonCorretto() {
+  fraseNonCorretto.innerText = sbagliati + "/10 questions";
+}
+
+risultatoConSlashNonCorretto();
+
+let testoCentro = document.querySelector("#centerText");
+
+function testoAlCentro() {
+  if (correct > sbagliati) {
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+    let p3 = document.createElement("p");
+
+    p1.innerText = "Congratulations!";
+    p2.innerText = "You passed the exam";
+    p3.innerText =
+      "We'll send you the certificate in a few minutes. Check your email (including promotions / spam folder)";
+
+    testoCentro.appendChild(p1);
+    testoCentro.appendChild(p2);
+    testoCentro.appendChild(p3);
+  } else {
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+    let p3 = document.createElement("p");
+
+    p1.innerText = "Better luck next time!";
+    p2.innerText = "You failed the exam";
+    p3.innerText =
+      "Check-in with your teaching assistant for more informations on how to proceed from here.";
+
+    testoCentro.appendChild(p1);
+    testoCentro.appendChild(p2);
+    testoCentro.appendChild(p3);
   }
-  
-  testoAlCentro();
-  
+}
+
+testoAlCentro();
